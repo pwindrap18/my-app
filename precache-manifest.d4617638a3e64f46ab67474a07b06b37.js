1bec58b2ee93f6d0f0277e70b44ee1a56e6e41be
pwindrap18 <pwindrap18@gmail.com>
2020-06-21 20:30:09 +0700

    Updates

---
+++ precache-manifest.d4617638a3e64f46ab67474a07b06b37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032df43f251c26f9055676a70583e6f5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8eb260d72fc98cfee791",
+    "url": "/my-app/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/my-app/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb260d72fc98cfee791",
+    "url": "/my-app/static/js/main.6372de39.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
